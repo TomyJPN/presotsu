6242a50af87e3e96eb319753f49e543c5675fdf6
TomyJPN <masaya120331@gmail.com>
2019-01-18 15:57:23 +0900

    wago.txtの「だらける」を手動修正、名詞と用言でネガポジ判定できるように

--- test.py
+++ test.py
@@ -24,13 +24,12 @@ def mecab_list(text):
         node = node.next
     return word_class
 
-
+point=0
+data=[]
+#日本語評価極性辞書（用言編）ver.1.0
 f=open(r'wago.txt','r',encoding="utf-8_sig")
 lines1=f.readlines()    #一行毎にファイル終端まで
-data=[]
 f.close()
-
-#日本語評価極性辞書（用言編）ver.1.0
 print("np判定用データの読み込み(用言)")
 # lines1: リスト。要素は1行の文字列データ
 for i,line in enumerate(lines1):
@@ -38,9 +37,40 @@ for i,line in enumerate(lines1):
     lines1[i] = line.replace('\n','')
     tmp = line.split()
     tmp.pop(0)
-    bl='ポジ' in line   #検索(あるならtrue)
+    if('ポジ' in line):   #検索(あるならtrue)
+        bl=1
+    else:
+        bl=-1
     data.append([bl,tmp[0]])
-    print(data[i])
+    #print(data[i])
+
+#日本語評価極性辞書（名詞編）ver.1.0
+f=open(r'pn.csv','r',encoding="utf-8_sig")
+lines2=f.readlines()    #一行毎にファイル終端まで
+f.close()
+print("np判定用データの読み込み(名詞)")
+for i,line in enumerate(lines2):
+    lines2[i] = line.replace('\r','')
+    lines2[i] = line.replace('\n','')
+    tmp = line.split()
+    if(tmp[1]=='p'):
+        #print('p:')
+        #print(tmp)
+        bl=1
+    elif(tmp[1]=='e'):
+        #print('e:')
+        #print(tmp)
+        bl=0
+    elif(tmp[1]=='n'):
+        #print('n:')
+        #print(tmp)
+        bl=-1
+    else:
+        #print('その他：')
+        #print(tmp)
+        bl=0
+    data.append([bl,tmp[0]])
+    #print(data[i])
 
 
 print("\n解析する文章を入力>>")
@@ -50,9 +80,20 @@ print(lst)
 print("ーーーー分解ーーーー")
 for i in lst:
     print(i[0])
-print("ーーーー原型ーーーー")
+print("ーーーー照合ーーーー")
 for i,item in enumerate(lst):
-    print(lst[i][4])
+    #print(lst[i][4])
     for j,word in enumerate(data):
         if(lst[i][4]==data[j][1]):
+            point+=data[j][0]
             print(data[j])
+            break   #被りを防ぐ
+
+print('\nネガポジ度')
+print(point)
+if(point>0):
+    print('ポジティブな文章です')
+elif(point<0):
+    print('ネガティブな文章です')
+else:
+    print('ネガポジどちらともいえません')
